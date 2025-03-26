d8f8eb8c7139e191211c68ec58b5df0b9a30e826
Arthur <arthur_o_soares@estudante.sesisenai.org.br>
2025-03-26 08:24:46 -0300

    UPDATE - Atividade 12 feita

--- script.js
+++ script.js
@@ -12,3 +12,13 @@ function verificarSinal(n1) {
 
 let n1 = parseFloat(prompt("Digite um valor"))
 verificarSinal(n1)
+
+// Atividade 12
+
+function converterTemperatura(temp){
+    let tempconvertido = (temp * 9/5) + 32
+    console.log(temp + "°C em fahrenheit é de " + tempconvertido + "°F")
+}
+
+let temp = parseFloat(prompt("Digite a temperatura em Graus Celsius"))
+converterTemperatura(temp)
